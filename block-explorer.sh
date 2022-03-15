0ad88ed9e58b4f46bf55b8ce6b952473237c3521
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-14 21:10:51 -0400

    Update block-explorer.sh

--- block-explorer.sh
+++ block-explorer.sh
@@ -11,34 +11,23 @@ echo "---------------"
 echo "installing hth"
 echo
 
-sudo apt-get -y install \
-      build-essential pkg-config libc6-dev m4 g++-multilib \
-      autoconf libtool ncurses-dev unzip git python \
-      zlib1g-dev wget bsdmainutils automake curl
+#source ./installer/ubuntu.sh
+sudo apt update -y # && sudo apt -y upgrade
+sudo apt install -y wget curl git
+sudo apt install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils dh-autoreconf
+sudo apt install -y libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+
+# If you don't have enough RAM (i.e. on Digital Ocean) you'll get an error like this:
+  # "g++: internal compiler error: Killed (program cc1plus)"
+  # so it's best to just go ahead and allocate some swap before the compile
+  # truncate -s 2048M /tmp.swap
+  sudo fallocate -l 2G ./tmp.swap
+  sudo mkswap ./tmp.swap
+  sudo chmod 0600 ./tmp.swap
+  sudo swapon ./tmp.swap
 
 # download koto source
-git clone https://github.com/HTHcoin/helpthehomelesscoin.git
-
-cd helpthehomelesscoin
-
-# apply bitcore patch
-git checkout v1.1.0
-patch -p1 < ./zcutil/bitcore.diff
-
-# download proving parameters
-./zcutil/fetch-params.sh
-
-# build patched koto
-./zcutil/build.sh -j 2
-
-# install lintian
-sudo apt-get -y install lintian
-
-# package koto
-./zcutil/build-debian-package.sh
-
-# install koto
-sudo dpkg -i koto-1.1.0-*.deb
+git clone https://github.com/HTHcoin/helpthehomelesscoin.git && cd helpthehomelesscoin && PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') && sudo bash -c "echo 0 > /proc/sys/fs/binfmt_misc/status" && cd depends && make -j7 && cd .. && ./autogen.sh && ./configure --prefix=/ --with-gui=no --with-incomplete-bdb --disable-tests && make -j7
 
 echo "---------------"
 echo "installing node and npm"
@@ -60,4 +49,3 @@ export NVM_DIR="$HOME/.nvm"
 
 echo "logout of this shell, log back in and run:"
 echo "bash block-explorer-part2.sh"
-
